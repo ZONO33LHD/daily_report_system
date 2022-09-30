f756fa2941413e4927f8cf6d2620d692d8905a91
nakazono <nakazono@localhost>
2022-09-30 20:22:37 +0900

    従業員indexアクションの作成

--- src/main/java/constants/MessageConst.java
+++ src/main/java/constants/MessageConst.java
@@ -1,4 +1,5 @@
 package constants;
+
 /**
  * 各出力メッセージを定義するEnumクラス
  *
@@ -17,11 +18,12 @@ public enum MessageConst {
 
     //バリデーション
     E_NONAME("氏名を入力してください。"),
-    E_NOPASSWARD("パスワードを入力してください。"),
+    E_NOPASSWORD("パスワードを入力してください。"),
     E_NOEMP_CODE("社員番号を入力してください。"),
     E_EMP_CODE_EXIST("入力された社員番号の情報は既に存在しています。"),
     E_NOTITLE("タイトルを入力してください。"),
     E_NOCONTENT("内容を入力してください。");
+
 
     /**
      * 文字列
@@ -36,13 +38,9 @@ public enum MessageConst {
     }
 
     /**
-     * 値（文字列）取得
+     * 値(文字列)取得
      */
     public String getMessage() {
         return this.text;
     }
-
-
-
-
 }
